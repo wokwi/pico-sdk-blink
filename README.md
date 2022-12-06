e3ecbf1db370581073887b1cdbc1ad33c189f0d9
Uri Shaked <uri@urishaked.com>
2022-12-06 11:45:35 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pico SDK Blink - Wokwi for VSCode
+
+Blink example using the Pico SDK with Wokwi configuration
+
+## Building
+
+```
+mkdir -p build
+cd build
+cmake ..
+make -j4
+```
+
+
+## Simulating
+
+Simulate this project using the Wokwi VSCode extension. Press "F1" and select "Wokwi: Start Simulator"
---
+++ blink.cpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+
+const uint LED_PIN = 2;
+
+int main() 
+{
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+    return 0;
+}
